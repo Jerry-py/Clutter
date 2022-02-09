0943bdc80e7c06b1381ee23e7b66369c9b218fc8
RGBCube <78925721+RGBCube@users.noreply.github.com>
2022-02-09 18:51:50 +0300

    nothing

--- utils/content/embed_assembler.py
+++ utils/content/embed_assembler.py
@@ -1,5 +1,3 @@
-from typing import List
-
 import discord
 
 from .mongo_manager import MongoManager
@@ -16,34 +14,26 @@ class EmbedAssembler:
     def _get_color(self, id: str, color: str) -> int:
         return self.db.get(f"{id}.colors.{color}", discord.Embed.Empty)
 
-    def success(self, title: str, description: str = "", *, id, fields: List[dict] = []) -> discord.Embed:
+    def success(self, title: str, description: str = "", *, id) -> discord.Embed:
         emoji, color = self._get_emoji(id, "success"), self._get_color(id, "success")
         _embed = discord.Embed(
             title=f"{emoji} {title}", description=description, color=color)
-        for field in fields:
-            _embed.add_field(name=field.get("title", ""), value=field.get("value", ""))
         return _embed
 
-    def error(self, title: str, description: str = "", *, id, fields: List[dict] = []) -> discord.Embed:
+    def error(self, title: str, description: str = "", *, id) -> discord.Embed:
         emoji, color = self._get_emoji(id, "error"), self._get_color(id, "error")
         _embed = discord.Embed(
             title=f"{emoji} {title}", description=description, color=color)
-        for field in fields:
-            _embed.add_field(name=field.get("title", ""), value=field.get("value", ""))
         return _embed
 
-    def warning(self, title: str, description: str = "", *, id, fields: List[dict] = []) -> discord.Embed:
+    def warning(self, title: str, description: str = "", *, id) -> discord.Embed:
         emoji, color = self._get_emoji(id, "warning"), self._get_color(id, "warning")
         _embed = discord.Embed(
             title=f"{emoji} {title}", description=description, color=color)
-        for field in fields:
-            _embed.add_field(name=field.get("title", ""), value=field.get("value", ""))
         return _embed
 
-    def info(self, title: str, description: str = "", *, id, fields: List[dict] = []) -> discord.Embed:
+    def info(self, title: str, description: str = "", *, id) -> discord.Embed:
         emoji, color = self._get_emoji(id, "info"), self._get_color(id, "info")
         _embed = discord.Embed(
             title=f"{emoji} {title}", description=description, color=color)
-        for field in fields:
-            _embed.add_field(name=field.get("title", ""), value=field.get("value", ""))
         return _embed
