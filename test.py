935515311b5381d999221212222612dc18ac52f9
RGBCube <78925721+RGBCube@users.noreply.github.com>
2022-02-19 14:00:08 +0300

    horrifying code to test workflows

--- test.py
+++ test.py
@@ -4,9 +4,8 @@ LisT = ReQues_t.get("https://google.com").text
 
 abc = []
 for i in LisT:
-  print(i)
-  abc.append(i)
-  if abc.count(i) > 1:
-    abc.remove(i)
-    print("dupe: {}".format(i))
-    
+    print(i)
+    abc.append(i)
+    if abc.count(i) > 1:
+        abc.remove(i)
+        print(f"dupe: {i}")
